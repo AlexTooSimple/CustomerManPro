9b10c16ebb629cb86fad7c976cbc1ff27a983f14
AlexHui1988 <hebewu1988@126.com>
2015-06-12 18:24:09 +0800

    no message

---
+++ HZAsiaPro/HZAsiaPro/SearchConditionView.h
@@ -0,0 +1,13 @@
+//
+//  SearchConditionView.h
+//  HZAsiaPro
+//
+//  Created by wuhui on 15/6/11.
+//  Copyright (c) 2015年 wuhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchConditionView : UIView
+
+@end
---
+++ HZAsiaPro/HZAsiaPro/SearchConditionView.m
@@ -0,0 +1,25 @@
+//
+//  SearchConditionView.m
+//  HZAsiaPro
+//
+//  Created by wuhui on 15/6/11.
+//  Copyright (c) 2015年 wuhui. All rights reserved.
+//
+
+#import "SearchConditionView.h"
+
+@implementation SearchConditionView
+
+-(id)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
+}
+
+- (void)layoutContentView
+{
+    
+}
+@end
