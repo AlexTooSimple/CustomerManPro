7e5d97554839f8e489f03aff9daa94254b928aed
AlexHui1988 <hebewu1988@126.com>
2015-06-25 16:30:18 +0800

    安全管理主页面

---
+++ HZAsiaPro/HZAsiaPro/SafeManageView.m
@@ -0,0 +1,21 @@
+//
+//  SafeManageView.m
+//  HZAsiaPro
+//
+//  Created by wuhui on 15/6/23.
+//  Copyright (c) 2015年 wuhui. All rights reserved.
+//
+
+#import "SafeManageView.h"
+
+@implementation SafeManageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
